94cb831bbd937d912b5a9432c80b3c24e663c5b8
conan <aconanlai@gmail.com>
2017-01-06 16:17:25 -0500

    enable CORS

--- index.js
+++ index.js
@@ -10,13 +10,19 @@ let db;
 
 app.use(bodyParser.urlencoded({extended: true}))
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', (req, res) => {
   db.collection('msgs').find().toArray(function(err, results) {
     res.json(results);
   });
 });
 
-// curl -d "param1=value1&param2=value2" -X POST https://regimen-server-gmiwwmvptm.now.sh
+// curl -d "param1=value1&param2=value2" -X POST https://regimen-server-quauxdvexe.now.sh/
 app.post('/', (req, res) => {
   db.collection('msgs').save(req.body, (err, result) => {
     if (err) return console.log(err)
